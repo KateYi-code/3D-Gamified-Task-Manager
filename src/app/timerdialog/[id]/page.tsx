83684c1660064295276fff0c7ba48b1e63b2c4cd
Tom Yuan <yuankui64@gmail.com>
2025-05-14 19:15:58 +1200

    Fix nextjs compile error (#98)
    fix nextjs error

--- src/app/timerdialog/[id]/page.tsx
+++ src/app/timerdialog/[id]/page.tsx
@@ -2,8 +2,13 @@
 
 import { TaskTimerDialog } from "@/components/task/TaskTimerDialog";
 import { useParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function TimerDialogPage() {
-    const { id } = useParams(); // 获取动态路由参数
-    return <TaskTimerDialog taskId={id as string} />;
+  const { id } = useParams(); // 获取动态路由参数
+  return (
+    <Suspense>
+      <TaskTimerDialog taskId={id as string} />
+    </Suspense>
+  );
 }
--- src/app/timerdialog/page.tsx
+++ src/app/timerdialog/page.tsx
@@ -2,11 +2,16 @@
 
 import { TaskTimerDialog } from "@/components/task/TaskTimerDialog";
 import { usePathname } from "next/navigation";
+import { Suspense } from "react";
 
 export default function TimerDialogPage() {
-    const pathname = usePathname();
-    // 从路径中提取taskId（假设路径格式为 /timerdialog/[taskId]）
-    const taskId = pathname.split("/").pop() || "";
+  const pathname = usePathname();
+  // 从路径中提取taskId（假设路径格式为 /timerdialog/[taskId]）
+  const taskId = pathname.split("/").pop() || "";
 
-    return <TaskTimerDialog taskId={taskId} />;
+  return (
+    <Suspense>
+      <TaskTimerDialog taskId={taskId} />
+    </Suspense>
+  );
 }
