582c3395aaec416f4cc86cb7fc3f6a718f7fe9e7
Tom Yuan <yuankui64@gmail.com>
2025-05-15 19:23:44 +1200

    fix user id (#133)

--- src/components/profile/ProfileContent.tsx
+++ src/components/profile/ProfileContent.tsx
@@ -31,7 +31,7 @@ export default function ProfileContent({ id }: Props) {
   return (
     <div className="flex flex-col items-center h-[100%] py-10 w-full self-center md:w-[800px] lg:w-[1000px] xl:w-[1200px]">
       {isMe && <CreatePostButton />}
-      <PostsList userIds={[user.id]} />
+      <PostsList userIds={[id]} />
     </div>
   );
 }
